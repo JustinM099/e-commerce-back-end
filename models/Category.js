50805fbbab182f1d76c304a019790d5d254ed761
JustinM099 <justinm099@mac.com>
2021-12-12 17:14:48 -0800

    creating models

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,24 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
   },
+
   {
     sequelize,
     timestamps: false,
